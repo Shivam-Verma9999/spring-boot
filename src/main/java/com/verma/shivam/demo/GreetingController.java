8e40c43f743acf3775e6a5a5accbff88f17f2222
Shivam Verma <sv805388@gmail.com>
2020-06-06 23:56:40 +0530

    custom aop

--- src/main/java/com/verma/shivam/demo/GreetingController.java
+++ src/main/java/com/verma/shivam/demo/GreetingController.java
@@ -1,4 +1,19 @@
 package com.verma.shivam.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello world :)";
+    }
+
+    @Loggable
+    @GetMapping("/greet")
+    public String greet(){
+        return "In the greet mapping";
+    }
 }
